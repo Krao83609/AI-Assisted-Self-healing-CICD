0c12de7369397febf8f42faa6364aff805dd81a9
Krao83609 <krao83609@gmail.com>
2026-06-28 02:10:04 +0530

    intentionally failing index.js

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ if (require.main === module) {
 
 module.exports = app; */
 
-nsole.log('Server is running from hell at port 007 with superNatural Powers')
+sole.log('Server is running from hell at port 007 with superNatural Powers')
